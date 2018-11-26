b0007d5fca5cdb18b963b65737587efffc3e494a
Matheus Almeida <mat.almeida@live.com>
2018-11-26 18:59:30 -0300

    feat: new return type

--- app.py
+++ app.py
@@ -8,7 +8,33 @@ api = Api(app)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    request_format = [
+        {"label": 1, "links": [2, 3]},
+        {"label": 2, "links": [1, 3]},
+        {"label": 3, "links": [1, 2]}
+    ]
+    response_format = [
+        {
+            "label": 1,
+            "pos": [6, 0],
+            "links": [2, 3]
+        },
+        {
+            "label": 2,
+            "pos": [9, 8],
+            "links": [3]
+        },
+        {
+            "label": 3,
+            "pos": [1, 7],
+            "links": []
+        }
+    ]
+    return {
+        "message": "Make a POST to this URL with the following format and passing the max size eg: url/graph?size=9",
+        "request-format": request_format,
+        "response-format": response_format
+    }
 
 
 api.add_resource(graphPosition, '/graph')
