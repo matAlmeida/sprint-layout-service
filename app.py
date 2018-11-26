c4fea6558a40cdde0d615abadab32014e12901de
Matheus Almeida <mat.almeida@live.com>
2018-11-26 19:46:07 -0300

    refac: None comparation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, request
-from flask_restful import Resource, Api
+from flask import Flask
+from flask_restful import Api
 from routes.graphPosition import graphPosition
 
 app = Flask(__name__)
@@ -42,7 +42,7 @@ api.add_resource(graphPosition, '/graph')
 
 if __name__ == '__main__':
     port = os.environ['PORT']
-    if port == None:
+    if port is None:
         app.run(debug=True)
     else:
         app.run(port=port)
